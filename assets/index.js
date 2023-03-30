cbb8e0745fbe26644819efd65997d21aa73fca6d
Belma Besirevic <72704864+belmab@users.noreply.github.com>
2023-03-30 16:06:55 +0200

    changed script

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,15 @@
-console.log('test');
+function openTab(tabName, button) {
+  const tabs = document.querySelectorAll('.tab')
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = 'none'
+  }
+
+  const buttons = document.getElementsByClassName('tab-btn')
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove('active')
+  }
+
+  document.getElementById(tabName).style.display = 'block'
+
+  button.classList.add('active')
+}
